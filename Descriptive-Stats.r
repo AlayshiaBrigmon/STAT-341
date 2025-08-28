35234cbde55570d66c4b9d910080167dd4d2f26d
AlayshiaBrigmon <albrigmon@claflin.edu>
2025-08-28 16:17:57 -0400

    Add files via upload

---
+++ Descriptive-Stats.r
@@ -0,0 +1,22 @@
+#Author:Alayshia Brigmon; Date: August 28, 2025; Purpose: Measures of descriptive stats
+
+# Generate dummy/sample dataset
+data <- c(1:50)
+
+# Calculate mean/average for data
+mean(data)
+
+# Calculate median for data
+median(data)
+
+# Calculate standard deviation for data
+sd(data)
+
+# Custom function
+get_mode <- function(x) {
+	uniq_x < -unique(x)
+	uniq_x[which.max(tabulate(match(x,uniq_x)))]}
+	data2 <-c(1,2,3,4,3,3)
+	get_mode(data2)
+	
+	
