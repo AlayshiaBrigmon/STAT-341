e1aafcf26dfb653c3d884a35cb6ab5b481d7c4ce
AlayshiaBrigmon <albrigmon@claflin.edu>
2025-09-04 16:22:03 -0400

    Add files via upload

---
+++ Correlation-Test.R
@@ -0,0 +1,8 @@
+#Author: Alayshia Brigmon, Date: 09/04/25, Purpose: Test The Correlation
+
+if(!require(devtools)) install.packages("devtools"); devtools::install_github("kassambara/ggpubr")
+
+# load the library ggpubr
+library (ggpubr)
+
+
